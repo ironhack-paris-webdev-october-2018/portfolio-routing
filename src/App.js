846b7699f2107e89fd8db8f8b69af52984b19b84
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-11-29 09:38:59 +0100

    Remove default content

--- src/App.js
+++ src/App.js
@@ -6,20 +6,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>Nizar's Portfolio Site</h1>
+
+          <nav>
+            Under construction...
+          </nav>
         </header>
+
+        Content that changes here...
+
+        <footer>
+          <p>Made with ⚙️ at Ironhack</p>
+        </footer>
       </div>
     );
   }
